8eb4198ad7d8beb144f9bc9c73eda89a15120b27
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2024-11-08 12:13:56 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary1: "#25AB75",
+      },
+    },
+  },
+  plugins: [],
+};
