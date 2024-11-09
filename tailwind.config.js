781870ca04d0028d467a91da266b464fd824a863
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2024-11-09 10:26:59 -0300

    feat: add blink animation and update primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,17 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      animation: {
+        blink: "blink 1s ease-in-out infinite",
+      },
+      keyframes: {
+        blink: {
+          "0%, 100%": { opacity: 1 },
+          "50%": { opacity: 0 },
+        },
+      },
       colors: {
-        primary1: "#25AB75",
+        primary1: "#fd9204",
       },
     },
   },
