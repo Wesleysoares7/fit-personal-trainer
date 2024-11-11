5d4e79aa060b82c3e0cf6865cf050b213278c1f9
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2024-11-11 11:39:28 -0300

    feat: adaptation for mobile devices

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,18 +1,59 @@
+import { useState, useEffect, useRef } from "react";
 import Button from "./button";
 import logo from "../assets/logo.png";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  const handleToggleMenu = () => setIsOpen(!isOpen);
+
+  // Função para fechar o menu se clicar fora dele
+  const handleClickOutside = (event: MouseEvent) => {
+    if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
   return (
     <div className="bg-black text-white h-full py-2 px-8">
       <nav className="flex justify-between items-center">
-        <div className="pl-20">
-          <img className="w-[25%]" src={logo} alt="" />
+        {/* Logo */}
+        <div className="md:pl-20">
+          <img className="w-[25%]" src={logo} alt="Logo" />
         </div>
-        <div className="flex gap-12 items-center">
-          <a href="home">Home</a>
-          <a href="home">Exercicios</a>
-          <a href="home">Treinos</a>
-          <a href="home">Planos</a>
+
+        {/* Botão de menu para mobile */}
+        <div className="lg:hidden">
+          <button onClick={handleToggleMenu} className="text-white">
+            &#9776; {/* Ícone de hambúrguer */}
+          </button>
+        </div>
+
+        {/* Links do menu */}
+        <div
+          ref={menuRef}
+          className={`${
+            isOpen ? "block" : "hidden"
+          } lg:flex lg:gap-12 lg:items-center absolute lg:static top-16 left-0 bg-black w-full lg:w-auto p-4 lg:p-0`}
+        >
+          <a href="home" className="block lg:inline">
+            Home
+          </a>
+          <a href="exercicios" className="block lg:inline">
+            Exercícios
+          </a>
+          <a href="treinos" className="block lg:inline">
+            Treinos
+          </a>
+          <a href="planos" className="block lg:inline">
+            Planos
+          </a>
           <div>
             <Button label="Contato" onClick={() => alert("Clicou!")} />
           </div>
