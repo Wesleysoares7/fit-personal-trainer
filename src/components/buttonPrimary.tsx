ab6ae98b5e0214f27065ae1ab798dff5ad574958
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2024-11-12 11:52:07 -0300

    feat: create button-primary component

---
+++ src/components/buttonPrimary.tsx
@@ -0,0 +1,30 @@
+interface ButtonProps {
+  label: string;
+  onClick: () => void;
+  type?: "button" | "submit" | "reset";
+  disabled?: boolean;
+  className?: string;
+}
+
+const ButtonPrimary = ({
+  label,
+  onClick,
+  type = "button",
+  disabled = false,
+  className = "",
+}: ButtonProps) => {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
+      className={`w-full px-8 py-1 bg-primary1 text-white rounded ${className} ${
+        disabled ? "opacity-50 cursor-not-allowed" : ""
+      }`}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default ButtonPrimary;
